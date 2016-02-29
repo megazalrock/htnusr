f494edcfccc68bf7256ee1a32369639ecc09a27b
otto <otto@mgzl.jp>
2016-03-01 01:55:34 +0900

    fix

--- src/js/components/FeedItem.js
+++ src/js/components/FeedItem.js
@@ -10,18 +10,21 @@ export default class FeedItem extends React.Component{
 
 	roundNum(num, dig = 2){
 		var p = Math.pow( 10 , dig );
-		return Math.round( num * p ) / p ;
+		return Math.floor( num * p ) / p ;
 	}
 
 	render(){
+		var date = new Date(this.props.data.date);
+		var bookmarkUrl = 'http://b.hatena.ne.jp/entry/' + (this.props.data.link.match(/^https/) ? 's/' : '') + this.props.data.link.replace(/^https?:\/\//, '');
 		return(
 			<div className={'feedItem view-' + this.props.viewMode}>
 				<div className="footer">
-					<div className={'score ' + (this.props.data.score < 0 ? 'minus' : 'plus')}>{this.roundNum(this.props.data.score)}</div>
-					<div className="date">{this.props.data.date}</div>
+					<div className={'score ' + (this.props.data.score < 0 ? 'minus' : 'plus')}>{this.roundNum(this.props.data.score).toFixed(2)}</div>
+					<a href={bookmarkUrl} target="_blank" className='bookmarkCount'><span className="count">{this.props.data.bookmarkCount}</span><span className="usersText">users</span></a>
+					<time className="date" dateTime={this.props.data.date}>{date.toLocaleString()}</time>
 					<div className="category">{this.props.data.category}</div>
 				</div>
-				<div className="title"><a href={this.props.data.link} target="_blank">{this.props.data.title}</a><a href={'http://b.hatena.ne.jp/entry/' + (this.props.data.link.match(/^https/) ? 's/' : '') + this.props.data.link.replace(/^https?:\/\//, '')} target="_blank">{this.props.data.bookmarkCount}users</a></div>
+				<div className="title"><a href={this.props.data.link} target="_blank">{this.props.data.title}</a></div>
 				<div className="text">
 					<div className="description">
 						{this.props.data.description}
