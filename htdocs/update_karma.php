cf147b3bb088b88bf587cb194cc5c0d116dfdc72
otto <otto@mgzl.jp>
2016-03-03 04:44:37 +0900

    fix

--- htdocs/update_karma.php
+++ htdocs/update_karma.php
@@ -1,5 +1,9 @@
 <?php
 require_once (dirname(__FILE__) . '/../app/Users.php');
 $users = new Users();
-$queue_list = $users->update_users(100);
+$limit = $_REQUEST['limit'];
+if(!isset($limit)){
+	$limit = 100;
+}
+$queue_list = $users->update_users($limit);
 echo count($queue_list) . 'users updated';
