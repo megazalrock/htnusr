5f8b608cc67138842f783162dbea75103eaea3c1
otto <otto@mgzl.jp>
2016-03-03 06:10:56 +0900

    update用のphpを移動

--- htdocs/update_karma.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once (dirname(__FILE__) . '/../app/Users.php');
-$users = new Users();
-$limit = $_REQUEST['limit'];
-if(!isset($limit)){
-	$limit = 100;
-}
-$queue_list = $users->update_users($limit);
-echo count($queue_list) . 'users updated';
