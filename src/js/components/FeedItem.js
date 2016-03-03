c04b91a61e1089aaf29fa15ea1a1de803bd7fe6a
otto <otto@mgzl.jp>
2016-03-04 06:05:27 +0900

    scoreの表示色を値に応じてかえる

--- src/js/components/FeedItem.js
+++ src/js/components/FeedItem.js
@@ -41,10 +41,38 @@ export default class FeedItem extends React.Component{
 	render(){
 		var date = new Date(this.props.data.date * 1000);
 		var bookmarkUrl = 'http://b.hatena.ne.jp/entry/' + (this.props.data.link.match(/^https/) ? 's/' : '') + this.props.data.link.replace(/^https?:\/\//, '');
+		var scoreColor;
+		var scoreSaturation = Math.abs(this.state.score);
+		var s = 0;
+		if(100 < scoreSaturation){
+			scoreSaturation = 100;
+		}
+		var easing = (t, b, c, d) =>{
+			return c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b;
+		};
+
+		if(this.state.score < 0){ //minus
+			s = easing(scoreSaturation, 0, 1, 100);
+			s *= 100;
+			s = (50 < s) ? 50 : s;
+			s = this._roundNum(s, 0);
+			scoreColor = 'hsl(0, ' + s + '%, 50%)';
+		}else if(0 < this.state.score){ //plus
+			s = easing(scoreSaturation, 0, 1, 1000);
+			s *= 100;
+			s = this._roundNum(s, 0);
+			//console.log(s);
+			scoreColor = 'hsl(100, ' + s + '%, 50%)';
+		}else{
+			scoreColor = 'hsl(100, 0%, 50%)';
+		}
+		var scoreStyle = {
+			background: scoreColor
+		};
 		return(
 			<div className={'feedItem view-' + this.props.viewMode}>
 				<div className="footer">
-					<div className={'score ' + (this.state.score < 0 ? 'minus' : this.state.score > 0 ? 'plus' : 'zero')}>{this.state.score}</div>
+					<div style={scoreStyle} className="score">{this.state.score}</div>
 					<a href={bookmarkUrl} target="_blank" className='bookmarkCount'><span className="count">{this.state.bookmarkCount}</span><span className="usersText">users</span></a>
 					<time className="date" dateTime={this.props.data.date}>{date.toLocaleString()}</time>
 					<div className="category">{this.props.data.category}</div>
