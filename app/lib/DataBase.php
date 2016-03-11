448fcea73fe6a134cb42b5b0ccf68669d86d0254
otto <otto@mgzl.jp>
2016-03-11 19:32:54 +0900

    DBの文字コードをutf8mb4に変更

--- app/lib/DataBase.php
+++ app/lib/DataBase.php
@@ -14,13 +14,47 @@ Class DataBase{
 			$tables = $rs->fetchAll(PDO::FETCH_COLUMN);
 			if(!in_array('users', $tables)){
 				$sth = $dbh->prepare(
+					"CREATE TABLE `feed_hot` (
+						`id` char(40) CHARACTER SET utf8 NOT NULL DEFAULT '',
+						`title` longtext CHARACTER SET utf8,
+						`link` longtext CHARACTER SET utf8,
+						`description` longtext CHARACTER SET utf8,
+						`html` longtext CHARACTER SET utf8,
+						`date` int(11) unsigned DEFAULT NULL,
+						`category` longtext CHARACTER SET utf8,
+						`index` int(11) NOT NULL,
+						PRIMARY KEY (`id`),
+						UNIQUE KEY `UNIQUE` (`id`)
+					) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+				);
+				$sth->execute();
+			}
+			if(!in_array('feed_hot', $tables)){
+				$sth = $dbh->prepare(
+					"CREATE TABLE `feed_new` (
+						`id` char(40) CHARACTER SET utf8 NOT NULL DEFAULT '',
+						`title` longtext CHARACTER SET utf8,
+						`link` longtext CHARACTER SET utf8,
+						`description` longtext CHARACTER SET utf8,
+						`html` longtext CHARACTER SET utf8,
+						`date` int(11) unsigned DEFAULT NULL,
+						`category` longtext CHARACTER SET utf8,
+						`index` int(11) NOT NULL,
+						PRIMARY KEY (`id`),
+						UNIQUE KEY `UNIQUE` (`id`)
+					) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+				);
+				$sth->execute();
+			}
+			if(!in_array('feed_new', $tables)){
+				$sth = $dbh->prepare(
 					"CREATE TABLE `users` (
-						`name` tinytext NOT NULL,
-						`karma` float DEFAULT NULL,
-						`score` float DEFAULT NULL,
-						`score_log10` float DEFAULT NULL,
+						`name` tinytext CHARACTER SET utf8 NOT NULL,
+						`karma` double DEFAULT NULL,
+						`score` double DEFAULT NULL,
+						`score_log10` double DEFAULT NULL,
 						`last_updated` int(11) DEFAULT NULL,
-						`priority` varchar(255) NOT NULL DEFAULT '0',
+						`priority` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '0',
 						`star_yellow` int(11) DEFAULT NULL,
 						`star_green` int(11) DEFAULT NULL,
 						`star_red` int(11) DEFAULT NULL,
@@ -29,41 +63,7 @@ Class DataBase{
 						`followers` int(11) DEFAULT NULL,
 						PRIMARY KEY (`name`(32)),
 						UNIQUE KEY `UNIQUE` (`name`(32))
-					) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
-				);
-				$sth->execute();
-			}
-			if(!in_array('feed_hot', $tables)){
-				$sth = $dbh->prepare(
-					"CREATE TABLE `feed_hot` (
-						`id` char(40) NOT NULL DEFAULT '',
-						`title` longtext,
-						`link` longtext,
-						`description` longtext,
-						`html` longtext,
-						`date` int(11) unsigned DEFAULT NULL,
-						`category` longtext,
-						`index` int(11) NOT NULL,
-						PRIMARY KEY (`id`),
-						UNIQUE KEY `UNIQUE` (`id`)
-					) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
-				);
-				$sth->execute();
-			}
-			if(!in_array('feed_new', $tables)){
-				$sth = $dbh->prepare(
-					"CREATE TABLE `feed_new` (
-						`id` char(40) NOT NULL DEFAULT '',
-						`title` longtext,
-						`link` longtext,
-						`description` longtext,
-						`html` longtext,
-						`date` int(11) unsigned DEFAULT NULL,
-						`category` longtext,
-						`index` int(11) NOT NULL,
-						PRIMARY KEY (`id`),
-						UNIQUE KEY `UNIQUE` (`id`)
-					) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+					) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
 				);
 				$sth->execute();
 			}
@@ -71,10 +71,10 @@ Class DataBase{
 				$sth = $dbh->prepare(
 					"CREATE TABLE `users_statistics` (
 						`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-						`key` text,
-						`value` longtext,
+						`key` text CHARACTER SET utf8,
+						`value` longtext CHARACTER SET utf8,
 						PRIMARY KEY (`id`)
-					) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+					) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;"
 				);
 				$sth->execute();
 				$sth = $dbh->prepare('INSERT INTO ' . $this->statistics_table_name . ' (`key`) VALUES (:key)');
@@ -99,7 +99,7 @@ Class DataBase{
 
 	}
 	public function connection(){
-		$dsn = 'mysql:host=' . $this->config['host'] . ';dbname=' . $this->config['name'] . ';charset=utf8';
+		$dsn = 'mysql:host=' . $this->config['host'] . ';dbname=' . $this->config['name'] . ';charset=utf8mb4';
 		try{
 			$dbh = new PDO($dsn, $this->config['user'], $this->config['password']);
 			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
