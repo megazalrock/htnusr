f9aac72b56398cb8090d736ea138870d483a5f11
otto <otto@mgzl.jp>
2016-03-21 16:50:01 +0900

    ブコメ表示欄をコンポーネントとして分離

--- src/js/components/FeedItem.js
+++ src/js/components/FeedItem.js
@@ -1,33 +1,16 @@
-import $ from 'jquery';
 import _ from 'lodash';
 import React from 'react';
-import ReactDom from 'react-dom';
 import Users from '../Users';
 import StorageCache from '../StorageCache';
+import FeedItemBookmarkComments from './FeedItemBookmarkComments.js';
 export default class FeedItem extends React.Component{
 	constructor(props){
 		super(props);
-		this.bookmarkCommentsBtnTexts = {
-			show: 'ブックマークコメントを表示',
-			hide: 'ブックマークコメントを非表示',
-			loading: 'loading...'
-		};
 		this.state = {
 			bookmarkCount: null,
-			score: null,
-			isCommentsOpen: false,
-			bookmarkComments: [],
-			bookmarkCommentsHtml: null,
-			eid: null,
-			bookmarkCommentsBtnText: this.bookmarkCommentsBtnTexts['show']
+			score: null
 		};
 		this.users = new Users();
-		this.$window = null;
-		this.$body = null;
-		this.$feedItem = null;
-		this.$comment = null;
-		this.$wrapper = null;
-		this.isCommentsOpen = false;
 	}
 	
 	componentWillUnmount(){
@@ -68,117 +51,6 @@ export default class FeedItem extends React.Component{
 				});
 			}			
 		}
-	}
-
-	handleOnClickShowBookmarkCommentBtn(){
-		this.isCommentsOpen = !this.isCommentsOpen;
-		this.setState({
-			'isCommentsOpen' : this.isCommentsOpen
-		});
-
-		var toggleCommentBox = () => {
-			var $window = this.$window = this.$window || $(window);
-			var $body = this.$body = this.$body || $('body, html');
-			var $feedItem = this.$feedItem = this.$feedItem || $(ReactDom.findDOMNode(this));
-			var $comments = this.$comments = this.$comments || $feedItem.find('.bookmarkComments .comments');
-			var top = $feedItem.offset().top;
-			var duration = 500;
-			if(this.isCommentsOpen){
-				$feedItem.data('top', top);
-				$body
-					.animate({
-						scrollTop: top
-					},{
-						duration: duration
-					});
-				$comments.css({
-					maxHeight:  $window.height() - $feedItem.outerHeight(true),
-					height: 300
-				});
-				this.setState({
-					bookmarkCommentsBtnText: this.bookmarkCommentsBtnTexts['hide']
-				});
-			}else{
-				$comments.css({
-					height: 0
-				});
-				this.setState({
-					bookmarkCommentsBtnText: this.bookmarkCommentsBtnTexts['show']
-				});
-			}
-		};
-
-		var setBookmarkCommentHtml = () => {
-			var html = '';
-			if(this.state.bookmarkComments && this.state.bookmarkComments.length){
-				html = this.state.bookmarkComments.map((comment, key) => {
-					if(comment.comment.length){
-						key = this.state.eid + '-' + key;
-						var bhatena = 'http://b.hatena.ne.jp';
-						var userImage = ['http://cdn1.www.st-hatena.com/users', comment.user.slice(0, 2),comment.user, 'profile_l.gif'].join('/');
-						var date = new Date(comment.timestamp);
-						var commentDate = [date.getFullYear(), this._zeroPadding(date.getMonth() + 1, 2), this._zeroPadding(date.getDate(), 2)].join('');
-						var tags = comment.tags.map((tag, key) => {
-							var encodedTag = encodeURIComponent(tag);
-							return (
-								<a className="tag" key={key} href={[bhatena, comment.user, encodedTag].join('/')}>{tag}</a>
-							);
-						});
-						var commentLink = [bhatena, comment.user, ''].join('/') + commentDate + '#bookmark-' + commentDate;
-						return (
-							<div key={key} className="comment">
-								<a target="_blank" className="profileIcon" href={[bhatena, comment.user, ''].join('/')}><img src={userImage} alt={comment.user} title={comment.user} width="24" height="24" /></a>
-								<div className="commentBox">
-									<a target="_blank" className="name" href={commentLink}>{comment.user}</a>
-									<span className="commentBody">{comment.comment}</span>
-									<span className="tags">{tags}</span>
-								</div>
-								<div className="info">
-									<a target="_blank" href={[bhatena, 'entry', this.state.eid, 'comment', comment.user].join('/')} className="permalink" title="パーマリンク">リンク</a>
-									<a target="_blank" href={commentLink} className="timestamp">{comment.timestamp}</a>
-								</div>
-							</div>
-						);
-					}
-				});
-			}
-			if(!(html && _.compact(html).length)){
-				html = (<div className="noComment">コメントはありません</div>);
-			}
-			this.setState({
-				'bookmarkCommentsHtml': html
-			});
-		};
-
-		if(!this.state.bookmarkComments || !this.state.bookmarkComments.length){
-			this.setState({
-				bookmarkCommentsBtnText: this.bookmarkCommentsBtnTexts['loading']
-			});
-			this.users.getUsers(this.props.data.link)
-				.then((data) => {
-					this.setState({
-						bookmarkComments: data.data.bookmarks,
-						eid: data.data.eid
-					});
-					setBookmarkCommentHtml();
-					toggleCommentBox();
-				});
-		}else{
-			if(_.isNull(this.state.bookmarkCommentsHtml)){	
-				setBookmarkCommentHtml();
-			}
-			toggleCommentBox();
-		}
-
-		
-		
-
-
-
-		/*console.log(reactid.replace(/\.\d+\.\d+$/, ''));
-		this.$feedList*/
-		//this.$comment = $(ReactDom.findDOMNode(this)).find('[data-reactid="' + reactid + '"]').parent();
-
 	}
 
 	_roundNum(num, dig = 2){
@@ -272,10 +144,7 @@ export default class FeedItem extends React.Component{
 						{this.props.data.description}
 					</div>
 				</div>
-				<div className="bookmarkComments">
-					<div className={'showBookmarkCommentsBtn' + (this.state.isCommentsOpen ? ' selected' : '')} onClick={this.handleOnClickShowBookmarkCommentBtn.bind(this)}>{this.state.bookmarkCommentsBtnText}</div>
-					<div className={'comments' + (this.state.isCommentsOpen ? ' open' : '')}>{this.state.bookmarkCommentsHtml}</div>
-				</div>
+				<FeedItemBookmarkComments link={this.props.data.link} users={this.users} bookmarkComments={this.state.bookmarkComments}/>
 			</div>
 		);
 	}
