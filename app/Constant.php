34daa6858baed1651ea5ea02a13f206c2947f368
otto <otto@mgzl.jp>
2016-08-28 23:27:18 +0900

    update_infoもキャッシュをかませるように変更

--- app/Constant.php
+++ app/Constant.php
@@ -4,3 +4,5 @@ const FEED_CACHE_EXPIRES = 300;// 60 * 5;
 const USER_SCORE_CAHCE_DIR = '/score';
 const FEED_CACHE_DIR = '/feed';
 const FEED_NUM = 50;
+const UPDATEINFO_CACHE_EXPIRES = 604800; //60 * 60 * 24 * 7
+const UPDATEINFO_CACHE_DIR = '/updateinfo';
